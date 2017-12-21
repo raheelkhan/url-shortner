60e9aac435762dc2723494d95945fe8514c67aa3
Raheel Khan <raheelwp@gmail.com>
2017-12-22 02:18:22 +0800

    response helpers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,34 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param array $data
+     * @param int $status_code
+     * @return Illuminate\Http\Response;
+     */
+    protected function sendOkResponse(array $data, int $status_code = 200)
+    {
+        $response = [
+            'status' => 'success',
+            'data' => $data
+        ];
+
+        return response()->json($response, $status_code);
+    }
+
+    /**
+     * @param string $message
+     * @param int $status_code
+     * @return Illuminate\Http\Response;
+     */
+    protected function sendErrorResponse(string $message, int $status_code = 500)
+    {
+        $response = [
+            'status' => 'error',
+            'message' => $message
+        ];
+
+        return response()->json($message, $status_code);
+    }
 }
